e20aaea5915338e31f9b43d431d84ebb1f91ff3f
Ralph Gabuat <dajuanenonli@yahoo.com>
2015-07-23 15:58:26 -0700

    Added person object

---
+++ ClassBook/Person.h
@@ -0,0 +1,21 @@
+//
+//  Person.h
+//  ClassBook
+//
+//  Created by Student on 7/23/15.
+//  Copyright (c) 2015 Student_Ralph. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, retain) NSString *firstName;
+@property (nonatomic, retain) NSString *lastName;
+@property (nonatomic, retain) NSString *address;
+@property (nonatomic, retain) NSString *city;
+@property (nonatomic, retain) NSString *state;
+@property (nonatomic, retain) NSString *zip;
+@property (nonatomic, retain) NSString *mobile;
+
+@end
